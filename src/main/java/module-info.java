90c0ffdfc137fdcb82d1d2bd223cba9d940e5eda
LEPAPE-Lucas-24009890 <lucas.lepape.1@etu.univ-amu.fr>
2025-05-05 11:24:13 +0200

    Exercice 7 fini

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,8 @@ module tp.intro.javafx {
     exports com.example.partie1;
     opens com.example.partie3 to javafx.fxml;
     exports com.example.partie3;
+    opens fr.amu.iut.exercice7 to javafx.fxml;
+    exports fr.amu.iut.exercice7;
+    opens fr.amu.iut.exercice8 to javafx.fxml;
+    exports fr.amu.iut.exercice8;
 }
