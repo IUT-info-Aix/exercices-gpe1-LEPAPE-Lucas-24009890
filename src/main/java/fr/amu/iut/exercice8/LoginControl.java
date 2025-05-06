c6da613aad03e40a7b3937368d6dcdb6334966dd
LEPAPE-Lucas-24009890 <lucas.lepape.1@etu.univ-amu.fr>
2025-05-06 16:09:21 +0200

    Exercice 8 fini

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,35 @@
 package fr.amu.iut.exercice8;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class LoginControl extends GridPane {
 
+    private int nbEtoile;
+    private String mdp = "";
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    private PasswordField pwd;
+
+    @FXML
     private void okClicked() {
+        System.out.println(userName.getText());
+        nbEtoile = pwd.getText().length();
+        for (int i = 0; i < nbEtoile; i++) {
+            mdp = mdp + ("*");
+        }
+        System.out.println(mdp);
+        mdp = "";
     }
 
+    @FXML
     private void cancelClicked() {
+        userName.clear();
+        pwd.clear();
     }
 }
